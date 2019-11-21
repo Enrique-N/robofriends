dd8f989a9d666e574ce33d7f470ee3f07f93415d
Robert Alva <robert.alva@tranzact.net>
2019-11-20 23:32:34 -0500

    Updates

---
+++ precache-manifest.1ab403e6e34f399c63d9bb072f3eb06f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba6df28b9f0b58d5d3b3cd4d4bf30b5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1c8f718bff123dd5e98c",
+    "url": "/robofriends/static/css/main.ce31a0fd.chunk.css"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/js/2.7514f082.chunk.js"
+  },
+  {
+    "revision": "1c8f718bff123dd5e98c",
+    "url": "/robofriends/static/js/main.fe29122d.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
